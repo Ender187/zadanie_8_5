f056704ba9b619951f620fcd0370e15db98f3236
Ender187 <m.hiltawski@hotmail.com>
2017-04-27 13:28:10 +0200

    skrypt z podstawa trojkata

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('wpisz wielkość podstawy a');
+var h = prompt('wpisz wysokość h');
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
